9c264a3eaeef1e8e27475c7f24ce57ed0f88bba9
ojoesuff <ojoesuff@gmail.com>
2019-07-24 12:15:42 +0100

    Updated code for ship movement but still wrong

--- src/ship.js
+++ src/ship.js
@@ -23,22 +23,33 @@ export default class Ship {
     ctx.closePath();
     ctx.fillStyle = "black";
     ctx.fill();
-    console.log(`${this.position.x}, ${this.position.y}`);
-    console.log(this.radians);
+    // console.log(`${this.position.x}, ${this.position.y}`);
+    // console.log(`${this.centre.x}, ${this.centre.y}`);
   }
 
   update(deltaTime) {}
 
   rotateAntiClock() {
-    this.position.x =
-      (this.position.x - this.centre.x) * Math.cos(this.radians) -
-      (this.position.y - this.centre.y) * Math.sin(this.radians) +
-      this.centre.x;
+    // this.position.x =
+    //   (this.position.x - this.centre.x) * Math.cos(this.radians) -
+    //   (this.position.y - this.centre.y) * Math.sin(this.radians) +
+    //   this.centre.x;
 
-    this.position.y =
-      (this.position.x - this.centre.x) * Math.sin(this.radians) +
-      (this.position.y - this.centre.y) * Math.cos(this.radians) +
-      this.centre.y;
+    // this.position.y =
+    //   (this.position.x - this.centre.x) * Math.sin(this.radians) +
+    //   (this.position.y - this.centre.y) * Math.cos(this.radians) +
+    //   this.centre.y;
+    //FIRST TRANSLATE THE DIFFERENCE
+    let x1 = this.position.x - this.centre.x;
+    let y1 = this.position.y - this.centre.y;
+
+    //APPLY ROTATION
+    x1 = x1 * Math.cos(this.radians) - y1 * Math.sin(this.radians);
+    y1 = x1 * Math.sin(this.radians) + y1 * Math.cos(this.radians);
+
+    //TRANSLATE BACK
+    this.position.x = x1 + this.centre.x;
+    this.position.y = y1 + this.centre.y;
   }
 
   rotateClock() {}
