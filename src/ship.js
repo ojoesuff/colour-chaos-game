d89d81d309bc0506b54a08f25cf506e8d4ae07f8
ojoesuff <ojoesuff@gmail.com>
2019-07-25 11:36:34 +0100

    Ship rotating a bit better

--- src/ship.js
+++ src/ship.js
@@ -10,6 +10,18 @@ export default class Ship {
       x: this.position.x + this.width / 2,
       y: this.position.y + this.height / 2
     };
+    this.point1 = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y
+    };
+    this.point2 = {
+      x: this.position.x,
+      y: this.position.y + this.height
+    };
+    this.point3 = {
+      x: this.position.x + this.width,
+      y: this.position.y
+    };
     this.degrees = 10;
     this.radians = (this.degrees * Math.PI) / 180;
   }
@@ -23,33 +35,22 @@ export default class Ship {
     ctx.closePath();
     ctx.fillStyle = "black";
     ctx.fill();
-    // console.log(`${this.position.x}, ${this.position.y}`);
+    console.log(`${this.position.x}, ${this.position.y}`);
     // console.log(`${this.centre.x}, ${this.centre.y}`);
   }
 
   update(deltaTime) {}
 
   rotateAntiClock() {
-    // this.position.x =
-    //   (this.position.x - this.centre.x) * Math.cos(this.radians) -
-    //   (this.position.y - this.centre.y) * Math.sin(this.radians) +
-    //   this.centre.x;
-
-    // this.position.y =
-    //   (this.position.x - this.centre.x) * Math.sin(this.radians) +
-    //   (this.position.y - this.centre.y) * Math.cos(this.radians) +
-    //   this.centre.y;
     //FIRST TRANSLATE THE DIFFERENCE
     let x1 = this.position.x - this.centre.x;
     let y1 = this.position.y - this.centre.y;
 
     //APPLY ROTATION
-    x1 = x1 * Math.cos(this.radians) - y1 * Math.sin(this.radians);
-    y1 = x1 * Math.sin(this.radians) + y1 * Math.cos(this.radians);
-
-    //TRANSLATE BACK
-    this.position.x = x1 + this.centre.x;
-    this.position.y = y1 + this.centre.y;
+    this.position.x =
+      x1 * Math.cos(this.radians) - y1 * Math.sin(this.radians) + this.centre.x;
+    this.position.y =
+      x1 * Math.sin(this.radians) + y1 * Math.cos(this.radians) + this.centre.y;
   }
 
   rotateClock() {}
